c4cb51fb7102e49cbdc3fd0aa15c6751e5c35026
Vasyl Hryts <vasylhryts@knu.ua>
2022-06-23 15:21:21 +0300

    route pages, title for all page,active page(light)

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
    2. Connect handlebars
    3. Update Layout, materialize(this is for update all page in 1 place)
    4. Add navigation(navbar, style, buty)
+   5. Rendering, work with pages courses and add.hbs, title for all pages, active page(add lighting)
--- index.js
+++ index.js
@@ -14,16 +14,32 @@ app.engine('hbs', hbs.engine)
 //а тут ми цей движок використовуєм
 app.set('view engine', 'hbs')
 app.set('views', 'views')
+
 //щоб зробити папку статичною і її експрес бачив
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   //тепер можна просто рендерити сторінки
-  res.render('index')
+  res.render('index', {
+    title: 'Головна сторінка',
+    //підсвітка коли на цій сторінці
+    isHome: true,
+  })
 })
 
-app.get('/about', (req, res) => {
-  res.render('about')
+//routes для сторінок
+app.get('/add', (req, res) => {
+  res.render('add', {
+    title: 'Додати',
+    isAdd: true,
+  })
+})
+
+app.get('/courses', (req, res) => {
+  res.render('courses', {
+    title: 'Курси',
+    isCourses: true,
+  })
 })
 
 const PORT = process.env.PORT || 3000
